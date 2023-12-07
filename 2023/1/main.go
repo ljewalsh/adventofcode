d92346c357bd7f834dfd30bb444c2424624b691a
Lucy-Jane Walsh <ljewalsh@gmail.com>
2023-12-07 15:13:07 -0300

    Format 2023/1 code

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "bufio"
-    "regexp"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 )
 
 func evaluateLine(line string) int {
-  r := regexp.MustCompile(`\d`)
-  numbersInLine := r.FindAllString(line, -1)
-  firstNumber, _ := strconv.Atoi(numbersInLine[0])
-  lastNumber, _ := strconv.Atoi(numbersInLine[len(numbersInLine) - 1])
-  return firstNumber * 10 + lastNumber
+	r := regexp.MustCompile(`\d`)
+	numbersInLine := r.FindAllString(line, -1)
+	firstNumber, _ := strconv.Atoi(numbersInLine[0])
+	lastNumber, _ := strconv.Atoi(numbersInLine[len(numbersInLine)-1])
+	return firstNumber*10 + lastNumber
 }
 
-func main(){
-  file, err := os.Open("./input.txt")
-  if err != nil {
-    os.Exit(1)
-  }
-  defer file.Close()
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  
-  var total int
+	scanner := bufio.NewScanner(file)
 
-  for scanner.Scan() {
-    total += evaluateLine(scanner.Text())
-  }
+	var total int
 
-  fmt.Println(total)
+	for scanner.Scan() {
+		total += evaluateLine(scanner.Text())
+	}
+
+	fmt.Println(total)
 }
